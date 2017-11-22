13ac0313b530ad200a18f3e82fed14c96fa58f0e
jiangliu2u <jiangliu2u@gmail.com>
2017-11-22 13:35:28 +0800

    hah

--- MyDiary/urls.py
+++ MyDiary/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^diary/', include('diary.urls')),
+	url(r'^diary/', include('diary.urls')),
+	url(r'^$',include('diary.urls')),
 ]
