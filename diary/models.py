b3f773b42dee6826339205aba87aed59afa5c21b
jiangliu2u <jiangliu2u@gmail.com>
2017-11-26 21:02:48 +0800

    调整Diary表结构

--- diary/models.py
+++ diary/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
-
-# Create your models here.
 
 class Diary(models.Model):
+    user = models.ForeignKey(User, default='')
     content = models.CharField(max_length=1000)
     pub_date = models.DateTimeField('date_published')
     mood = models.CharField(max_length=10)
